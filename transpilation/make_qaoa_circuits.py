16204259947eec7f960174cd4b5ab07de281a4a5
Daniel Egger <deg@zurich.ibm.com>
2025-08-13 15:10:19 +0200

    * Better handling of cost_layer

--- transpilation/make_qaoa_circuits.py
+++ transpilation/make_qaoa_circuits.py
@@ -24,6 +24,7 @@ from qiskit.transpiler.passes import (
     InverseCancellation
 )
 
+from qaoa_training_pipeline.pre_processing import SATMapper
 from qaoa_training_pipeline.utils.problem_classes import MaxCut, MaxIndependentSet
 from qaoa_training_pipeline.utils.data_utils import load_input
 from qaoa_training_pipeline.utils.graph_utils import dict_to_graph
@@ -33,11 +34,44 @@ from qopt_best_practices.transpilation.qaoa_construction_pass import QAOAConstru
 from qopt_best_practices.transpilation.swap_cancellation_pass import SwapToFinalMapping
 
 
+from qiskit.transpiler import TransformationPass
+
+class PrepareCostLayer(TransformationPass):
+    """Prepares the cost layer for the `Commuting2qGateRouter`.
+    
+    TODO: this should move to qopt-best-practices.
+    """
+
+    def run(self, dag):
+        """Run the pass."""
+        commuting_nodes = []
+        for node in dag.topological_op_nodes():
+            if node.op.name not in ["rz", "rzz"]:
+                raise ValueError(
+                    f"{self.__class__.__name__} only supports rz and rzz nodes. "
+                    f"Found {node.op.name} instead."
+                )
+
+            if node.op.name == "rzz":
+                commuting_nodes.append(node)
+
+        commuting_block = Commuting2qBlock(commuting_nodes)
+
+        wire_order = {
+            wire: idx
+            for idx, wire in enumerate(dag.qubits)
+            if wire not in dag.idle_wires()
+        }
+
+        dag.replace_block_with_op(commuting_nodes, commuting_block, wire_order)
+
+
 def make_qaoa_circuit(
     file_name: str, 
     problem_class: str, 
-    turn_off_sat: bool = False, 
-    backend = None,
+    reps: int,
+    backend,
+    sat_timeout: int = 60, 
 ):
     """Specify a problem instance by name and make a hardware native QAOA circuit.
     
@@ -46,6 +80,11 @@ def make_qaoa_circuit(
     2. Erdos-Renyi graphs are SAT mapped and stranspiled to a line with a swap strategy.
     3. Hardware native heavy-hex graphs are mapped directly to the heavy-hex hardware.
     4. Graphs from a line to fully connected are directly implemented with a swap strategy.
+
+    Args:
+        file_name: The name of the file in which there is a graph to load.
+        problem_class: Either `maxcut` or `mis:X` where `X` is the value of the penalty.
+        reps: The number of QAOA layers.
     """
 
     # 1. Load and pre-process data into a cost-operator.
@@ -64,13 +103,16 @@ def make_qaoa_circuit(
         raise ValueError("Invalid graph type.")
 
     # 1.1 SAT map problems that need SAT mapping
+    sat_mapper = None
     if graph_type in ["random_regular", "erdos_renyi"]:
-        raise NotImplementedError("SAT mapping not implemented yet.")
+        if sat_timeout > 0:
+            sat_mapper = SATMapper(timeout=sat_timeout)
+            input_data = sat_mapper(input_data)
 
     if problem_class == "maxcut":
         cost_op = MaxCut().cost_operator(input_data)
     elif problem_class[0:3] == "mis":
-        mis = MaxIndependentSet.from_str(problem_class)
+        mis = MaxIndependentSet.from_str(problem_class[4:])
         cost_op = mis.cost_operator(input_data)
     else:
         raise ValueError("Invalid problem class.")
@@ -118,11 +160,12 @@ def make_qaoa_circuit(
 
         swap_strat = SwapStrategy(cmap, ())  # no SWAPs needed
     else:
-        edge_coloring.update({(idx, idx + 1): idx % 2 for idx in range(num_qubits - 1)})
+        edge_coloring = {(idx, idx + 1): idx % 2 for idx in range(num_qubits - 1)}
 
         swap_strat = SwapStrategy.from_line(range(num_qubits))
 
     ## 3.2 Find a qubit layout that is good. (TODO)
+    layout_info = {}
     if graph_type == "heavy_hex":
         initial_layout = None
         raise NotImplementedError("Hex finding is not yet supported.")
@@ -130,12 +173,18 @@ def make_qaoa_circuit(
         path_finder = BackendEvaluator(backend)
         path, fidelity, num_subsets = path_finder.evaluate(num_qubits)
         initial_layout = Layout.from_intlist(path, cost_layer.qregs[0])
+        layout_info = {
+            "path": path,
+            "fidelity": fidelity,
+            "num_subsets": num_subsets,
+        }
 
     ## 3.3 Construct the pass manager
 
     # Apply the SWAP strategy to the cost layer.
     pre_init = PassManager(
         [
+            PrepareCostLayer(),
             Commuting2qGateRouter(swap_strat, edge_coloring),
             SwapToFinalMapping(),  # Removes unnecessary SWAP gates that the end of the block
             HighLevelSynthesis(basis_gates=["x", "cx", "sx", "rz", "id"]),
@@ -153,17 +202,26 @@ def make_qaoa_circuit(
 
     staged_pm = generate_preset_pass_manager(3, backend, initial_layout=initial_layout)
     staged_pm.pre_init = pre_init
-    staged_pm.init = PassManager([QAOAConstructionPass(reps=3)])
+    staged_pm.init = PassManager([QAOAConstructionPass(num_layers=reps)])
     staged_pm.post_init = post_init
 
-    dag_circ = circuit_to_dag(cost_layer)
-    instruction = Commuting2qBlock(list(dag_circ.topological_op_nodes()))
+    ## 3.4 Run the pass manager
+    isa_circuit = staged_pm.run(cost_layer)
 
-    block_qaoa_ansatz = QuantumCircuit(num_qubits)
-    block_qaoa_ansatz.compose(instruction, inplace=True)
+    isa_circuit.metadata["file_name"] = file_name
+    isa_circuit.metadata["problem_class"] = problem_class
+    isa_circuit.metadata["layout_info"] = layout_info
 
-    isa_circuit = staged_pm.run(block_qaoa_ansatz)
+    if sat_mapper is not None:
+        isa_circuit.metadata["sat mapper"] = sat_mapper.to_config()
 
-    # TODO: do some validation
+    # 4. Validation TODO
+    validation_pm = PassManager(
+        [
+            PrepareCostLayer(),
+            Commuting2qGateRouter(swap_strat, edge_coloring),
+            SwapToFinalMapping(),
+        ]
+    )
 
-    return isa_circuit
+    return isa_circuit, validation_pm.run(cost_layer)
